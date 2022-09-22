4a00bbc45d95a8c00310c081b9517ab9d65c2a93
haidohong <dohaimail@gmail.com>
2022-09-22 16:59:00 +0700

    ANOTHER CROSSOVER METHOD: Seems to yeild better solution than the previous

--- GA/OOP_GA.cpp
+++ GA/OOP_GA.cpp
@@ -27,6 +27,14 @@ vector<int> generate_new_individual(int gen_length){
     random_shuffle ( new_gene.begin(), new_gene.end() );
     return new_gene;
 }
+vector<int> generate_order(vector<int> &arr){
+    int arr_sz = arr.size();
+    vector<int> order(arr_sz);
+    for (int i = 0; i < arr_sz; ++i){
+        order[arr[i]] = i;
+    }
+    return order;
+}
 class Individual{
 public:
     vector<int> chromosome;
@@ -95,7 +103,8 @@ public:
     void selection();
     void produce_offspring();
     void produce_mutation();
-    Individual crossover(Individual p1, Individual p2);
+    Individual crossover_1(Individual p1, Individual p2);
+    Individual crossover_2(Individual p1, Individual p2);
     void show_population();
     void show_population_();
 };
@@ -104,7 +113,7 @@ Population::Population(int population_sz, int gen_length){
         this->population.push_back(Individual(gen_length));
     }
 }
-Individual Population::crossover(Individual parent1, Individual parent2){
+Individual Population::crossover_1(Individual parent1, Individual parent2){
     int gen_length = parent1.chromosome.size();
     int pos1 = rand() % gen_length, pos2 = rand() % gen_length;
     if (pos1 > pos2) swap(pos1, pos2);
@@ -133,13 +142,36 @@ Individual Population::crossover(Individual parent1, Individual parent2){
     }
     return Individual(child);
 }
+Individual Population::crossover_2(Individual parent1, Individual parent2){
+    int gen_length = parent1.chromosome.size();
+    vector<int> child(gen_length, 0);
+    vector<bool> visited(gen_length, false);
+    vector<int> order = generate_order(parent1.chromosome);
+    int take_from = 0;
+    for (int i = 0; i < gen_length; ++i){
+        if (visited[i]) continue;
+        visited[i] = true;
+        int idx = i;
+        while (parent2.chromosome[idx] != parent1.chromosome[i]){
+            visited[idx] = true;
+            if (take_from) child[idx] = parent2.chromosome[idx];
+            else child[idx] = parent1.chromosome[idx];
+            idx = order[parent2.chromosome[idx]];
+            visited[idx] = true;
+        }
+        if (take_from) child[idx] = parent2.chromosome[idx];
+        else child[idx] = parent1.chromosome[idx];
+        take_from = 1 - take_from;
+    }
+    return Individual(child);
+}
 void Population::produce_offspring(){
     int parent_range = this->population.size();
     for (int i = 0; i < parent_range; ++i){
         for (int j = i + 1; j < parent_range; ++j){
             double curent_rate = ((double) rand() / (RAND_MAX));
             if (curent_rate <= CROSSOVER_RATE){
-                Individual new_child = this->crossover(this->population[i], this->population[j]);
+                Individual new_child = this->crossover_2(this->population[i], this->population[j]);
                 this->population.push_back(new_child);
             }
         }
